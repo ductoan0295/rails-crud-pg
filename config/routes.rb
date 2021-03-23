56172d51a51cfd167366fa64226e03064fd0a392
toan <ductoan0295@gmail.com>
2021-03-24 01:06:49 +0700

    digest password

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
+  resources :users
   get "/" => "session#create"
   post "/session" => "session#create"
-  # post "/users" => "users#create"
+  post "/users" => "users#create"
   get "/todoitems" => "todoitems#index"
   
   resources :todoitems
-  resources :users
 end
